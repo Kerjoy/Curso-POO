2b8ea4fa448dbcd0f87d64dde18e2ba1b7d2a456
JC <jc_sr31@outlook.com>
2024-04-16 23:51:06 -0600

    POO Course

---
+++ Introducion a POO.py
@@ -0,0 +1,11 @@
+#Introducion a POO
+#Por que necesitamos la programacion orientada a objetos?
+#Es una forma de programar que usualmente se usa en proyectos muy grandes
+
+#Ejemplo de videjuego: En un juego de cases hay relaciones entre personas objetos y caractaristicas
+
+celular1_marca = "Samsung"
+celular2_marca = "Apple"
+celular3_marca = "Huawei"
+
+celular1_modelo = "S23"
