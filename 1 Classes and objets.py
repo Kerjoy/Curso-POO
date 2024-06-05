8a19051f2e2fa1f2c95e826a77f6b0292518c98a
JC <jc_sr31@outlook.com>
2024-06-04 22:10:25 -0600

    1 classes

---
+++ 1 Classes and objets.py
@@ -0,0 +1,14 @@
+#three methods for naming
+#thisCamelCase
+#this_snake_case
+#ThisPascalCase
+class Cellphone():
+    #static attributes
+    brand = "Samsumg"
+    model = "S23"
+    camera = "48MP"
+    
+cell = Cellphone()
+print(cell.camera)
+    
+    
