dc79f9974c04beb500631f9677ea54abd5e6ab69
JC <jc_sr31@outlook.com>
2024-06-04 21:47:47 -0600

    incorrect ways for creating values in objects

--- Introducion a POO.py
+++ Introducion a POO.py
@@ -9,3 +9,13 @@ celular2_marca = "Apple"
 celular3_marca = "Huawei"
 
 celular1_modelo = "S23"
+celular2_modelo = "Iphone"
+celular3_modelo = "P20 PRO"
+
+celular1_camaraT = "24MP"
+celular2_camaraT = "224MP"
+celular2_camaraT = "124MP"
+
+celular1_camaraF = "24MP"
+celular2_camaraF = "224MP"
+celular2_camaraF = "124MP"
