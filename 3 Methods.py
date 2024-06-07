fcb453767885e68125ee84c3d7fae5883cec99e3
JC <jc_sr31@outlook.com>
2024-06-06 20:30:15 -0600

    3 Methods.py

---
+++ 3 Methods.py
@@ -0,0 +1,13 @@
+# a method is a function on class
+
+class cellphone:
+    def __init__(self,brand, model,camera):
+        self.brand = brand
+        self.model = model
+        self.camera = camera
+    def this_is_method():
+        pass
+    
+cell1 = cellphone ("Samgung","S23","48MP")
+
+print(cell1.brand)
