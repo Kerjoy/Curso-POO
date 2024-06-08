ac5380c399f4f55a90bdd8e2c40c18b1c33e42fa
JC <jc_sr31@outlook.com>
2024-06-07 22:51:05 -0600

    exercise 1.1

---
+++ 4 Exercises 1.py
@@ -0,0 +1,13 @@
+class student:
+    def __init__(self,name,old,grade):
+        self.name = name
+        self.old = old
+        self.grade = grade
+        
+pedro = student("MR CROC",24,100)
+
+name = input("introduce your name please: ")
+old = input("introduce your name old: ")
+name = input("introduce your name grade: ")
+
+student1 = student(name,old,name)
