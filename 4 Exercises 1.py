e5d6f22d5cd73b29a826ad04bf99511effa87dd7
JC <jc_sr31@outlook.com>
2024-06-12 23:28:47 -0600

    adding last lines

--- 4 Exercises 1.py
+++ 4 Exercises 1.py
@@ -4,6 +4,9 @@ class student:
         self.old = old
         self.grade = grade
         
+    def studyng(self):
+        print("Estudyng...")
+        
 pedro = student("MR CROC",24,100)
 
 name = input("introduce your name please: ")
@@ -11,3 +14,13 @@ old = input("introduce your name old: ")
 name = input("introduce your name grade: ")
 
 student1 = student(name,old,name)
+
+print(f"""
+     STUDENT DATA: \n\n
+     Name: {student.name} \n
+     Edad: {student.old} \n
+     Grade: {student.grade} \n
+    
+    """)
+
+studyng = input()
