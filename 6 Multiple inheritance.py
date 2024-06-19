1a52539994b232f4fc8bf099d5188902eaf60ea3
JC <jc_sr31@outlook.com>
2024-06-18 22:26:08 -0600

    need corrections

--- 6 Multiple inheritance.py
+++ 6 Multiple inheritance.py
@@ -7,3 +7,22 @@ class People:
     def method_speak(self):
         print("probe i can speak anything")
         
+class WitchHunter:
+    def __init__(self,skill):
+            self.skill = skill
+    def show_skill(self):
+            print(f"My skill is: {self.show_skill}")
+        
+class MercenaryWitchHunter(People,WitchHunter):
+    def __init__(self,name,age,nationality,skill,reward,company):
+        People.__init__(self,name,age,nationality)
+        WitchHunter.__init__(self,skill)
+        self.reward = reward
+        self.company = company
+    
+    def show(self):
+        print(f'{self.show_skill()}')
+    
+mr_salary_man = MercenaryWitchHunter("paco",22,"czech","crafting of potions",1500,"tink")
+
+mr_salary_man.show()
