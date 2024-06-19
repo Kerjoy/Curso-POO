ea0829bbee9ce007d61d6ede9b78502eece0219c
JC <jc_sr31@outlook.com>
2024-06-18 21:09:55 -0600

    inheritance example

---
+++ 6 Multiple inheritance.py
@@ -0,0 +1,9 @@
+class People:
+    def __init__(self,name,age,nationality):
+        self.name = name
+        self.age = age
+        self.nationality = nationality
+
+    def method_speak(self):
+        print("probe i can speak anything")
+        
